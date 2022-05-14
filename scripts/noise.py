f3db8208d222d00b1651fece71d3b3deb92b7bb4
Korneliusz Osmenda <korneliuszo@gmail.com>
2022-05-14 21:52:03 +0200

    Noise temporal dithering

---
+++ scripts/noise.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    import sys
+    import argparse
+    parser = argparse.ArgumentParser(description='Display image')
+    parser.add_argument("--ip",'-i', help='ip')
+    parser.add_argument("--image",'-I', help='image')
+
+    args = parser.parse_args()
+    if (not args.ip):
+        print("Provide ip")
+        sys.exit(1)
+    if (not args.image):
+        print("Provide image")
+        sys.exit(1)
+        
+    from cga import cga_06
+    from PIL import Image
+    import monitor
+    import numpy as np
+    from time import sleep
+    
+    i = Image.open(args.image)
+    cga = cga_06(monitor.monitor(args.ip))
+    
+    i=i.resize(cga.resolution())
+    w, h=cga.resolution()
+    i=i.convert("L")
+    while True:
+        noise =  Image.fromarray(np.random.randint(0,255,(h,w),dtype=np.dtype('uint8')),mode="L")
+        im = Image.blend(i,noise,0.4).convert("1",dither=Image.Dither.NONE)
+        cga.display(im) 
+        
