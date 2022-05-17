0fb2d87c1abf64426aba8afbf6c73361737a8eee
Korneliusz Osmenda <korneliuszo@gmail.com>
2022-05-17 19:03:10 +0200

    player WIP

---
+++ scripts/player.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from timebudget import timebudget
+timebudget.set_quiet()  # don't show measurements as they happen
+timebudget.report_at_exit()  # Generate report when the program exits
+
+if __name__ == "__main__":
+    import sys
+    import argparse
+    parser = argparse.ArgumentParser(description='Display image')
+    parser.add_argument("--ip",'-i', help='ip')
+    parser.add_argument("--image",'-I', help='image')
+    parser.add_argument("--type",'-t', help='monitor type')
+   
+    args = parser.parse_args()
+    if (not args.ip):
+        print("Provide ip")
+        sys.exit(1)
+    if (not args.image):
+        print("Provide image")
+        sys.exit(1)
+    if (not args.type):
+        args.type = "cga"  
+        
+    from cga import cga_06
+    from hga import hga
+    from PIL import Image
+    import monitor 
+    import cv2
+    
+    cga = {"cga":cga_06,
+            "hga": hga}[args.type](args.ip)
+    
+    cap = cv2.VideoCapture(args.image)
+
+    while True:
+        ret, frame = cap.read()
+        if ret == False:
+            break
+        color_coverted = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        i = Image.fromarray(color_coverted)
+        i=i.resize(cga.resolution())
+        cga.display(i.convert("1",dither=Image.Dither.NONE))
+    
+    
+    
+    
