84572997f119eb6f7acd32ead6b8b170c4375ffb
Korneliusz Osmenda <korneliuszo@gmail.com>
2023-06-21 21:05:38 +0200

    Add drive number

--- scripts/inmememu.py
+++ scripts/inmememu.py
@@ -9,6 +9,8 @@ class inmememu(int13handler.int13h):
 
     def get_chs(self):
         return chssize.chssize(len(self.image))
+    def get_size(self):
+        return len(self.image)
     def read(self,sector,len):
         return self.image[sector*512:(sector+len)*512]
     def write(self,sector,data):
@@ -22,6 +24,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='floppy image')
     parser.add_argument("--ip",'-i', help='ip')
     parser.add_argument("--image",'-I', help='image')
+    parser.add_argument("--drive",'-d', help='drive',type=lambda x: int(x,0),default=0)
     
     args = parser.parse_args()
     if (not args.ip):
@@ -32,13 +35,18 @@ if __name__ == "__main__":
         sys.exit(1)
 
     m=monitor.monitor(args.ip)
-    fdd=inmememu(m,0x00,args.image)
+    fdd=inmememu(m,args.drive,args.image)
+    if(args.drive&0x80):
+        data = bytearray(m.getmem(0,0x475,1))
+        data[0]+=1;
+        m.putmem(0,0x475,data)
+
     m.install_13h()
     m.continue_boot()
     while True:
         m.wait_for_isr()
         params=m.get_called_params()
         if fdd.process(params):
-            continue
+            continue  
         m.continue_boot()
     
